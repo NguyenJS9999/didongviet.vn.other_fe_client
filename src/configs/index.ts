e67b5d7433d6fc521989461cee611b8bf037a84a
Nguyễn Trung Nguyên <31590813+NguyenKeo@users.noreply.github.com>
2024-03-06 16:07:13 +0700

    push itnit didongviet.vn.other_backend

---
+++ src/configs/index.ts
@@ -0,0 +1,15 @@
+export type Provider = 'google' | 'github';
+export const providers: Provider[] = ['google', 'github'];
+
+// export const SERVER_URL = 'http://192.168.0.110:3000';
+export const SERVER_URL = 'http://localhost:3001';
+
+export const redirectLinks = {
+	google: '/auth/google',
+	github: '/auth/github',
+};
+
+export const accessLinks = {
+	google: '/auth/google/callback',
+	github: '/auth/github/callback',
+};
